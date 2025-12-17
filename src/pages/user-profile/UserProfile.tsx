3742e2e8b48a4641ad64c9b3a3914963b205246e
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2025-12-17 23:22:07 +0200

    add filtring to userprofiler page

--- src/pages/user-profile/UserProfile.tsx
+++ src/pages/user-profile/UserProfile.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import classNames from "classnames";
 import { useParams } from "react-router";
 import { useUser } from "../../shared/hooks/queries/useUser";
 import { useRecipes } from "../../shared/hooks/queries/useRecipes";
 import RecipesGrid from "../../shared/components/recipes-grid/RecipesGrid";
 import ProfileAvatar from "../../shared/components/profile-avatar/ProfileAvatar";
+import style from "./UserProfile.module.scss";
 
 const UserProfile = () => {
+  const [activeTab, setActiveTab] = useState<"recipes" | "favorites">(
+    "recipes",
+  );
   const userId = useParams().userId;
   const userData = useUser(userId);
 
@@ -21,9 +27,27 @@ const UserProfile = () => {
     return <div>Помилка завантаження профілю користувача.</div>;
   }
 
+  const getActiveIndex = () => {
+    return activeTab === "recipes" ? 0 : 1;
+  };
+
   return (
     <div>
       {userData.data && <ProfileAvatar userData={userData.data} />}
+      <div className={style.buttons} data-active={getActiveIndex()}>
+        <button
+          className={style.button}
+          onClick={() => setActiveTab("recipes")}
+        >
+          Рецепти
+        </button>
+        <button
+          className={style.button}
+          onClick={() => setActiveTab("favorites")}
+        >
+          Улюблені
+        </button>
+      </div>
       <RecipesGrid recipes={recipes.data?.recipesList || []} />
     </div>
   );
